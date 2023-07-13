725ea475fd5edc169b428358c18b8c3d520f7ad4
lowtierre <lorenzoterreni1997@gmail.com>
2023-07-13 14:24:49 +0200

    update skill items

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const htmlTarget = 90;
 const cssTarget = 70;
 const jsTarget = 80;
+const githubTarget = 70;
+const pythonTarget = 90;
+const mongoTarget = 20;
 
 let percentHtmlString = document.getElementById("html")
 let percentCssString = document.getElementById("css")
 let percentJsString = document.getElementById("js")
+let percentGitHubString = document.getElementById("github")
+let percentPythonString = document.getElementById("python")
+let percentMongoString = document.getElementById("mongo")
 
 const addOne = (string, target, interval) => {
     console.log(target)
@@ -20,3 +26,6 @@ const addOne = (string, target, interval) => {
 let htmlInterval = setInterval(() => addOne(percentHtmlString, htmlTarget, htmlInterval), 10);
 let cssInterval = setInterval(() => addOne(percentCssString, cssTarget, cssInterval), 10);
 let jsInterval = setInterval(() => addOne(percentJsString, jsTarget, jsInterval), 10);
+let githubInterval = setInterval(() => addOne(percentGitHubString, githubTarget, githubInterval), 10);
+let pythonInterval = setInterval(() => addOne(percentPythonString, pythonTarget, pythonInterval), 10);
+let mongoInterval = setInterval(() => addOne(percentMongoString, mongoTarget, mongoInterval), 10);
